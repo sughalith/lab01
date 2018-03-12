fe9948580f613fa281f61c613928a1c94221ce1e
Arkadiusz Stankiewicz <sughalith@gmail.com>
2018-03-12 14:12:32 +0100

    crud impl

--- src/main/java/pl/arek/repository/UndeadRepositoryFactory.java
+++ src/main/java/pl/arek/repository/UndeadRepositoryFactory.java
@@ -1,8 +1,20 @@
 package pl.arek.repository;
 
+import pl.arek.service.UndeadManagerImpl;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class UndeadRepositoryFactory {
 
+
     public static UndeadRepository getInstance(){
-        return null;
+        try {
+            String url = "jdbc:hsqldb:hsql://localhost/workdb";
+            return new UndeadRepositoryImpl(DriverManager.getConnection(url));
+        }
+        catch (SQLException e){
+            return null;
+        }
     }
 }
--- src/test/java/pl/arek/undeadTests/domain/UndeadTest.java
+++ src/test/java/pl/arek/undeadTests/domain/UndeadTest.java
@@ -19,13 +19,13 @@ public class UndeadTest {
     }
 
     @Test
-    public void getById(){
+    public void getById() {
         Long idToFind = (long) 1;
         assertNotNull(undeadRepository.getById(idToFind));
     }
 
     @Test
-    public void addUndead(){
+    public void addUndead() {
         Undead zombie = new Undead();
         zombie.setId((long) 1);
         zombie.setType("Testowy 1");
@@ -35,39 +35,39 @@ public class UndeadTest {
     }
 
     @Test
-    public void deleteUndead(){
+    public void deleteUndead() {
         Undead zombie = undeadRepository.getById((long) 1);
         undeadRepository.deleteUndead(zombie);
-        if (undeadRepository.getAll().size() > 0){
+        if (undeadRepository.getAll().size() > 0) {
             assertNotNull(undeadRepository.getAll());
-        }else {
             assertNull(undeadRepository.getById(zombie.getId()));
+
         }
     }
 
     @Test
-    public void updateUndead(){
+    public void updateUndead() {
         Undead clicker = new Undead();
         clicker.setId((long) 1);
         clicker.setType("Clicker");
-        Long zombieToUpdate = (long)1;
+        Long zombieToUpdate = (long) 1;
         undeadRepository.updateUndead(zombieToUpdate, clicker);
         assertEquals(undeadRepository.getById(zombieToUpdate).getType(), clicker.getType());
 
-        for(Undead undead : undeadRepository.getAll()){
-            if(clicker.getId().equals(zombieToUpdate)) {
+        for (Undead undead : undeadRepository.getAll()) {
+            if (clicker.getId().equals(zombieToUpdate)) {
                 assertNotEquals(undead.getType(), clicker.getType());
             }
         }
     }
 
     @Test
-    public void getAll(){
-    assertNotNull(undeadRepository.getAll());
+    public void getAll() {
+        assertNotNull(undeadRepository.getAll());
     }
 
     @Before
-    public void initRepository(){
+    public void initRepository() {
         undeadRepository = UndeadRepositoryFactory.getInstance();
         Undead walker = new Undead();
         Undead ghoul = new Undead();
