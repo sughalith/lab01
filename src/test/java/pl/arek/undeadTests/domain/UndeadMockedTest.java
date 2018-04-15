a95a3eef363eec773ea1e9336609dd0a897fbbfd
Arkadiusz Stankiewicz <sughalith@gmail.com>
2018-04-15 21:04:26 +0200

    mocked

--- src/test/java/pl/arek/undeadTests/domain/UndeadMockedTest.java
+++ src/test/java/pl/arek/undeadTests/domain/UndeadMockedTest.java
@@ -12,6 +12,7 @@ import pl.arek.domain.Undead;
 import pl.arek.repository.UndeadRepository;
 import pl.arek.repository.UndeadRepositoryImpl;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.mockito.Mockito.*;
 
 import java.sql.Connection;
@@ -46,11 +47,23 @@ public class UndeadMockedTest {
     ResultSet resultSet;
 
 
-    @Ignore
     @Test
     public void getById() throws SQLException {
-        int idToFind = undeadRepository.getAll().size() - 1;
-        assertNotNull(undeadRepository.getById(idToFind));
+//        int idToFind = undeadRepository.getAll().size() - 1;
+//        assertNotNull(undeadRepository.getById(idToFind));
+        AbstractResultSetById mockedResultSet = mock(AbstractResultSetById.class);
+        when(mockedResultSet.next()).thenCallRealMethod();
+        when(mockedResultSet.getInt("id")).thenCallRealMethod();
+        when(mockedResultSet.getString("name")).thenCallRealMethod();
+        when(getUndeadByIdStmt.executeQuery()).thenReturn(mockedResultSet);
+
+        assertNotNull(undeadRepository.getById(1));
+
+        verify(getUndeadByIdStmt, times(1)).executeQuery();
+        verify(mockedResultSet, times(1)).getInt("id");
+        verify(mockedResultSet, times(1)).getString("name");
+        verify(mockedResultSet, times(1)).next();
+
     }
 
     @Test
@@ -79,16 +92,23 @@ public class UndeadMockedTest {
 
     }
 
-    @Ignore
     @Test
-    public void updateUndead() throws SQLException {
+    public void getAll() throws SQLException{
+        //assertNotNull(undeadRepository.getAll());
 
-    }
+        AbstractResultSet mockedResultSet = mock(AbstractResultSet.class);
+        when(mockedResultSet.next()).thenCallRealMethod();
+        when(mockedResultSet.getInt("id")).thenCallRealMethod();
+        when(mockedResultSet.getString("name")).thenCallRealMethod();
+        when(getAllUndeadsStmt.executeQuery()).thenReturn(mockedResultSet);
 
-    @Ignore
-    @Test
-    public void getAll() {
-        assertNotNull(undeadRepository.getAll());
+        assertEquals(1, undeadRepository.getAll().size());
+
+        verify(getAllUndeadsStmt, times(1)).executeQuery();
+        verify(mockedResultSet, times(1)).getInt("id");
+        verify(mockedResultSet, times(1)).getString("name");
+        verify(mockedResultSet, times(2)).next();
+
     }
 
     abstract class AbstractResultSet implements ResultSet {
@@ -113,24 +133,52 @@ public class UndeadMockedTest {
         }
     }
 
-    @Test
-    public void checkGetting() throws Exception {
-        when(connectionMock.prepareStatement("SELECT * FROM Undead")).thenReturn(getAllUndeadsStmt);
-        AbstractResultSet mockedResultSet = mock(AbstractResultSet.class, Mockito.CALLS_REAL_METHODS);
-        when(mockedResultSet.next()).thenCallRealMethod();
-        when(mockedResultSet.getInt("id")).thenCallRealMethod();
-        when(mockedResultSet.getString("type")).thenCallRealMethod();
-        when(getAllUndeadsStmt.executeQuery()).thenReturn(mockedResultSet);
+    abstract class AbstractResultSetById implements ResultSet {
+        int i = 0;
 
-        assertEquals(1, undeadRepository.getAll().size());
+        @Override
+        public int getInt(String s) throws SQLException {
+            return 1;
+        }
+
+        @Override
+        public String getString(String columnLabel) throws SQLException {
+            return "clicker";
+        }
+
+        @Override
+        public boolean next() throws SQLException {
+            if (i == 1)
+                return false;
+            i++;
+            return true;
+        }
     }
+    
+    @Ignore
+    @Test
+    public void updateUndead() throws SQLException {
 
+        Undead undead = new Undead();
+        undead.setId(1);
+        undead.setType("wariat");
+        undeadRepository.addUndead(undead);
+        Undead undeadToUpdate = undeadRepository.getById(undead.getId());
+        undeadToUpdate.setType("clicker");
+        undeadRepository.updateUndead(1, undeadToUpdate);
+
+        assertEquals(undeadRepository.getById(undead.getId()).getType(), undeadToUpdate.getType());
+        verify(updateUndeadStmt, times(1)).setString(1, "clicker");
+        verify(updateUndeadStmt).executeUpdate();
+
+
+    }
 
     @Before
     public void initRepository() throws SQLException {
         when(connectionMock.prepareStatement("INSERT INTO Undead (name) VALUES (?)")).thenReturn(addUndeadStmt);
-//        when(connectionMock.prepareStatement("SELECT id, name FROM Undead")).thenReturn(getAllUndeadsStmt);
-//        when(connectionMock.prepareStatement("SELECT id, name FROM Undead WHERE id = ?")).thenReturn(getUndeadByIdStmt);
+        when(connectionMock.prepareStatement("SELECT * FROM Undead")).thenReturn(getAllUndeadsStmt);
+        when(connectionMock.prepareStatement("SELECT * FROM Undead WHERE id = ?")).thenReturn(getUndeadByIdStmt);
         when(connectionMock.prepareStatement("UPDATE Undead SET name = ? WHERE id = ?")).thenReturn(updateUndeadStmt);
         when(connectionMock.prepareStatement("DELETE FROM Undead WHERE id = ?")).thenReturn(deleteUndeadStmt);
         when(connectionMock.prepareStatement("DROP TABLE Undead")).thenReturn(dropTableStmt);
